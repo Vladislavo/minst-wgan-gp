3903b39fc6c55c93105c32d24631fe5e6c600470
Vladislav Rykov <vladoss@list.ru>
2021-09-24 14:12:49 -0700

    losses and training

--- main.py
+++ main.py
@@ -188,7 +188,7 @@ def get_gradient(crit, real, fake, epsilon):
         gradient: the gradient of the critic's scores, with respect to the mixed image
     '''
     # Mix the images together
-    mixed_images = real * epsilon + fake * (1 - epsilon)
+    mixed_images = real*epsilon + fake*(1 - epsilon)
 
     # Calculate the critic's scores on the mixed images
     mixed_scores = crit(mixed_images)
@@ -221,10 +221,107 @@ def test_get_gradient(image_shape, device='cpu'):
     return gradient
 
 gradient = test_get_gradient((256, 1, 28, 28))
-print("Success!")
+print("Gradient Test: Success!")
 
+def gradient_penalty(gradient):
+    '''
+    Return the gradient penalty, given a gradient.
+    Given a batch of image gradients, you calculate the magnitude of each image's gradient
+    and penalize the mean quadratic distance of each magnitude to 1.
+    Parameters:
+        gradient: the gradient of the critic's scores, with respect to the mixed image
+    Returns:
+        penalty: the gradient penalty
+    '''
+    # Flatten the gradients so that each row captures one image
+    gradient = gradient.view(len(gradient), -1)
 
+    # Calculate the magnitude of every row
+    gradient_norm = gradient.norm(2, dim=1)
+    
+    # Penalize the mean squared distance of the gradient norms from 1
+    penalty = torch.mean(torch.pow(gradient_norm - 1, 2))
 
+    return penalty
+
+def test_gradient_penalty(image_shape):
+    bad_gradient = torch.zeros(*image_shape)
+    bad_gradient_penalty = gradient_penalty(bad_gradient)
+    assert torch.isclose(bad_gradient_penalty, torch.tensor(1.))
+
+    image_size = torch.prod(torch.Tensor(image_shape[1:]))
+    good_gradient = torch.ones(*image_shape) / torch.sqrt(image_size)
+    good_gradient_penalty = gradient_penalty(good_gradient)
+    assert torch.isclose(good_gradient_penalty, torch.tensor(0.))
+
+    random_gradient = test_get_gradient(image_shape)
+    random_gradient_penalty = gradient_penalty(random_gradient)
+    # c_lambda = 0.1, otherwise it is never asserted true.
+    # Anyway this condition does not make much sense, since 
+    #   the penalty may be higher or lower depending on
+    #   the predicted values.
+    assert random_gradient_penalty*0.1 < 0.1
+
+test_gradient_penalty((256, 1, 28, 28))
+print("Gradient Penalty Test: Success!")
+
+# LOSSES
+
+def get_gen_loss(crit_fake_pred):
+    '''
+    Return the loss of a generator given the critic's scores of the generator's fake images.
+    Parameters:
+        crit_fake_pred: the critic's scores of the fake images
+    Returns:
+        gen_loss: a scalar loss value for the current batch of the generator
+    '''
+    gen_loss = -torch.mean(crit_fake_pred)
+    return gen_loss
+
+def test_gen_loss():
+    assert torch.isclose(
+        get_gen_loss(torch.tensor(1.)), torch.tensor(-1.0)
+    )
+
+    assert torch.isclose(
+        get_gen_loss(torch.rand(10000)), torch.tensor(-0.5), 0.05
+    )
+
+    print("Generator Loss Test: Success!")
+
+test_gen_loss()
+
+def get_crit_loss(crit_fake_pred, crit_real_pred, gp, c_lambda):
+    '''
+    Return the loss of a critic given the critic's scores for fake and real images,
+    the gradient penalty, and gradient penalty weight.
+    Parameters:
+        crit_fake_pred: the critic's scores of the fake images
+        crit_real_pred: the critic's scores of the real images
+        gp: the unweighted gradient penalty
+        c_lambda: the current weight of the gradient penalty 
+    Returns:
+        crit_loss: a scalar for the critic's loss, accounting for the relevant factors
+    '''
+
+    # crit_loss = torch.mean(crit_real_pred)-torch.mean(crit_fake_pred)+c_lambda*gp
+    crit_loss = torch.mean(crit_fake_pred)-torch.mean(crit_real_pred)+c_lambda*gp
+
+    return crit_loss
+
+def test_crit_loss():
+    assert torch.isclose(
+        get_crit_loss(torch.tensor(1.), torch.tensor(2.), torch.tensor(3.), 0.1),
+        torch.tensor(-0.7)
+    )
+    assert torch.isclose(
+        get_crit_loss(torch.tensor(20.), torch.tensor(-20.), torch.tensor(2.), 10),
+        torch.tensor(60.)
+    )
+
+    print("Critic Loss Test: Success!")
+
+test_crit_loss()
 
 # TRAINING
 
@@ -238,7 +335,6 @@ def train():
     # - lr - learning rate
     # - beta_1, beta_2 - momentum terms
     # - device - 'cpu' or 'gpu'
-    criterion = nn.BCEWithLogitsLoss()
     n_epochs = 200
     z_dim = 64
     display_step = 1000
@@ -257,7 +353,7 @@ def train():
     ])
 
     dataloader = DataLoader(
-        MNIST('.', download=True, transform=transform), # download=True for the first time running
+        MNIST('.', download=False, transform=transform), # download=True for the first time running
         batch_size=batch_size,
         shuffle=True)
 
@@ -278,51 +374,75 @@ def train():
     gen = gen.apply(weights_init)
     crit = crit.apply(weights_init)
 
-    mean_generator_loss = 0
-    mean_discriminator_loss = 0
+    cur_step = 0
+    generator_losses = []
+    critic_losses = []
+
     for epoch in range(n_epochs):
-        # Dataloader returns the batches
         for real, _ in tqdm(dataloader):
             cur_batch_size = len(real)
             real = real.to(device)
 
-            ## Update discriminator ##
-            disc_opt.zero_grad()
-            fake_noise = get_noise(cur_batch_size, z_dim, device=device)
-            fake = gen(fake_noise)
-            disc_fake_pred = disc(fake.detach())
-            disc_fake_loss = criterion(disc_fake_pred, torch.zeros_like(disc_fake_pred))
-            disc_real_pred = disc(real)
-            disc_real_loss = criterion(disc_real_pred, torch.ones_like(disc_real_pred))
-            disc_loss = (disc_fake_loss + disc_real_loss) / 2
+            mean_iteration_critic_loss = 0
+            for _ in range(crit_repeats):
+                ### Update critic ###
+                crit_opt.zero_grad()
+                fake_noise = get_noise(cur_batch_size, z_dim, device=device)
+                fake = gen(fake_noise)
+                crit_fake_pred = crit(fake.detach())
+                crit_real_pred = crit(real)
 
-            # Keep track of the average discriminator loss
-            mean_discriminator_loss += disc_loss.item() / display_step
-            # Update gradients
-            disc_loss.backward(retain_graph=True)
-            # Update optimizer
-            disc_opt.step()
+                epsilon = torch.rand(len(real), 1, 1, 1, device=device, requires_grad=True)
+                gradient = get_gradient(crit, real, fake.detach(), epsilon)
+                gp = gradient_penalty(gradient)
+                crit_loss = get_crit_loss(crit_fake_pred, crit_real_pred, gp, c_lambda)
 
-            ## Update generator ##
+                # Keep track of the average critic loss in this batch
+                mean_iteration_critic_loss += crit_loss.item() / crit_repeats
+                # Update gradients
+                crit_loss.backward(retain_graph=True)
+                # Update optimizer
+                crit_opt.step()
+            critic_losses += [mean_iteration_critic_loss]
+
+            ### Update generator ###
             gen_opt.zero_grad()
             fake_noise_2 = get_noise(cur_batch_size, z_dim, device=device)
             fake_2 = gen(fake_noise_2)
-            disc_fake_pred = disc(fake_2)
-            gen_loss = criterion(disc_fake_pred, torch.ones_like(disc_fake_pred))
+            crit_fake_pred = crit(fake_2)
+            
+            gen_loss = get_gen_loss(crit_fake_pred)
             gen_loss.backward()
+
+            # Update the weights
             gen_opt.step()
 
             # Keep track of the average generator loss
-            mean_generator_loss += gen_loss.item() / display_step
+            generator_losses += [gen_loss.item()]
 
-            ## Visualization code ##
+            ### Visualization code ###
             if cur_step % display_step == 0 and cur_step > 0:
-                print(f"Step {cur_step}: Generator loss: {mean_generator_loss}, discriminator loss: {mean_discriminator_loss}")
+                gen_mean = sum(generator_losses[-display_step:]) / display_step
+                crit_mean = sum(critic_losses[-display_step:]) / display_step
+                print(f"Step {cur_step}: Generator loss: {gen_mean}, critic loss: {crit_mean}")
                 show_tensor_images(fake)
                 show_tensor_images(real)
-                mean_generator_loss = 0
-                mean_discriminator_loss = 0
+                step_bins = 20
+                num_examples = (len(generator_losses) // step_bins) * step_bins
+                plt.plot(
+                    range(num_examples // step_bins), 
+                    torch.Tensor(generator_losses[:num_examples]).view(-1, step_bins).mean(1),
+                    label="Generator Loss"
+                )
+                plt.plot(
+                    range(num_examples // step_bins), 
+                    torch.Tensor(critic_losses[:num_examples]).view(-1, step_bins).mean(1),
+                    label="Critic Loss"
+                )
+                plt.legend()
+                plt.show()
+
             cur_step += 1
 
-# if __name__ == '__main__':
-#     train()
+if __name__ == '__main__':
+    train()
